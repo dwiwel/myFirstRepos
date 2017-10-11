278c1599f61e9790e8eacc2aa9b51dfcdf9cd2c8
dwiwel <dwiwel@gmail.com>
2017-10-11 10:06:22 -0400

    new cmake stuff

---
+++ opencv/grabber/MyGrabber_CmakeProj/CMakeFiles/MyGrabber_CmakeProh.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyGrabber_CmakeProh.
+# This may be replaced when dependencies are built.
---
+++ opencv/grabber/MyGrabber_CmakeProj/MyGrabber_CmakeProh.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	std::cout << " Hello from myGrabber_CmakeProject. \n" ;
+}

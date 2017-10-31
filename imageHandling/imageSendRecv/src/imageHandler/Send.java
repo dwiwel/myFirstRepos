a3d60d3541f5aba3c7ed6c1f62c37ce1379fc368
Donald Wiwel <dwiwel@gmail.com>
2017-10-30 23:24:07 -0400

    new classes

---
+++ imageHandling/imageSendRecv/src/imageHandler/Send.java
@@ -0,0 +1,35 @@
+package imageHandler;
+
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.OutputStream;
+import java.net.*;
+
+class Send {
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 13085);
+        OutputStream outputStream = socket.getOutputStream();
+
+        BufferedImage image = ImageIO.read(new File("C:\\Users\\Jakub\\Pictures\\test.jpg"));
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", byteArrayOutputStream);
+
+        byte[] size = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array();
+        outputStream.write(size);
+        outputStream.write(byteArrayOutputStream.toByteArray());
+        outputStream.flush();
+        System.out.println("Flushed: " + System.currentTimeMillis());
+
+        Thread.sleep(120000);
+        System.out.println("Closing: " + System.currentTimeMillis());
+        socket.close();
+    }
+}

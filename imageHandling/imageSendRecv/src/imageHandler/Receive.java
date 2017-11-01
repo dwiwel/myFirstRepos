707b472aafd1743bce6eeac2089691793303daf4
Donald Wiwel <dwiwel@gmail.com>
2017-10-31 20:26:37 -0400

    adding filename

--- imageHandling/imageSendRecv/src/imageHandler/Receive.java
+++ imageHandling/imageSendRecv/src/imageHandler/Receive.java
@@ -24,34 +24,52 @@ public class Receive {
     	Boolean run = true;
     	
     	System.out.println("--Starting image receive ...");
-    	while (run)
+    	//while (run)
     	{
 	        ServerSocket serverSocket = new ServerSocket(13085);
 	        
 	        System.out.println("--Waiting for client ...");
 	        Socket socket = serverSocket.accept();
+	        socket.setSoTimeout(8000);
 	        InputStream inputStream = socket.getInputStream();
 	
-	        System.out.println("Reading: " + System.currentTimeMillis());
+	        System.out.println("Reading time: " + System.currentTimeMillis());
 	
-	        String fileName = "newImage.jpg";
-	        // byte[] fileName = new byte[32];
+	        String fileName = "newImage.jpg";      // New output file
+	        byte[] sizeFileName = new byte[4];        // Size of filename;
+
+	        inputStream.read(sizeFileName);
+	        int sizeFile = ByteBuffer.wrap(sizeFileName).asIntBuffer().get();
+	        System.out.println("Received FileName Size in bytes: " + sizeFile);
+	        byte[] fileNameArray = new byte[sizeFile];
+	       
+	        inputStream.read(fileNameArray);
+	        char fileNameFromClient = ByteBuffer.wrap(fileNameArray).asCharBuffer().get();
+	              
+	        System.out.println("Filename: " + fileNameFromClient);
 	        
-	        
-	        byte[] sizeAr = new byte[16];        // Size of image.
+	        byte[] sizeAr = new byte[4];        // Size of image in a byte array
+
 	        inputStream.read(sizeAr);
 	        int size = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
-	        System.out.println("Received Image Size: " + size);
+	        System.out.println("Received Image Size in bytes: " + size);
 	        byte[] imageAr = new byte[size];
+	       
 	        inputStream.read(imageAr);
-	
+		        
 	        BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageAr));
-	
-	        System.out.println("Received " + image.getHeight() + "x" + image.getWidth() + ": " + System.currentTimeMillis());
-	        ImageIO.write(image, "jpg", new File("C://data//images//" + fileName));
-	
+	        System.out.println("Received image dim: " + image.getHeight() + "x" + image.getWidth() + " time: " + System.currentTimeMillis());
+	        
+	        File fileOut = new File("C://data//images//" + fileName);
+	        		
+	        if (fileOut.exists()) {
+	        	fileOut.delete(); 
+	        }
+	      
+	        ImageIO.write(image, "jpg", fileOut);
+	        
 	        serverSocket.close();
     	}
-    	System.out.println("--Ending image receive ...");
+    	System.out.println("--End of image receive.");
     }
 }
--- imageHandling/imageSendRecv/src/imageHandler/Send.java
+++ imageHandling/imageSendRecv/src/imageHandler/Send.java
@@ -17,24 +17,47 @@ class Send {
     	
     	System.out.println("--Sending image ...");
         Socket socket = new Socket("localhost", 13085);
+        socket.setSoTimeout(8000);
         OutputStream outputStream = socket.getOutputStream();
 
-        BufferedImage image = ImageIO.read(new File("C:\\data\\images\\test.jpg"));
+        String inputImagePath = "C:\\data\\images\\test.jpg";
+        String outputImagePathThumb = "C:\\data\\images\\test_thumb.jpg";
+         
+        String fileName = "test_171030_1800.jpg";
+        byte[] fileNameByte = new byte[64];  
+        
+        fileNameByte = fileName.getBytes();
+        int fileNameLen = fileNameByte.length;
+        
+        byte[] sizeName = ByteBuffer.allocate(4).putInt(fileNameLen).array();
+        outputStream.write(sizeName);
+        outputStream.write(fileNameByte);
+        
+        // resize to a fixed width (not proportional)
+        int scaledWidth = 640;
+        int scaledHeight = 480;
+        ImageResizer.resize(inputImagePath, outputImagePathThumb, scaledWidth, scaledHeight);
 
+        BufferedImage image = ImageIO.read(new File(outputImagePathThumb));
+        
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         ImageIO.write(image, "jpg", byteArrayOutputStream);
 
-        byte[] size = ByteBuffer.allocate(16).putInt(byteArrayOutputStream.size()).array();
-        System.out.println("Send Image size: " + size);
+        int sizeByte = byteArrayOutputStream.size();       
+        
+        byte[] size = ByteBuffer.allocate(4).putInt(sizeByte).array();
+        System.out.println("Send Image size: " + byteArrayOutputStream.size());
         outputStream.write(size);
-        outputStream.write(byteArrayOutputStream.toByteArray());
+        
+        byte[] bytesSend = byteArrayOutputStream.toByteArray();
+        outputStream.write(bytesSend);
         outputStream.flush();
         System.out.println("Flushed: " + System.currentTimeMillis());
 
         Thread.sleep(1000);
         System.out.println("Closing: " + System.currentTimeMillis());
         socket.close();
-    	System.out.println("--Don sending image");
+    	System.out.println("--Done sending image");
 
     }
 }

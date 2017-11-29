745d6ea4f76af992b72f44803ef0efdafb2e2ad8
Donald Wiwel <dwiwel@gmail.com>
2017-11-29 23:07:16 +0000

    df

--- XBee/myFirstXBeeApp/src/com/digi/xbee/example/MyTCPReceiveListener.java
+++ XBee/myFirstXBeeApp/src/com/digi/xbee/example/MyTCPReceiveListener.java
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 package com.digi.xbee.example;
 
 import com.digi.xbee.api.listeners.IIPDataReceiveListener;
@@ -13,19 +14,4 @@ public class MyTCPReceiveListener implements IIPDataReceiveListener   {
 	}
 
 }
-=======
-package com.digi.xbee.example;
 
-import com.digi.xbee.api.listeners.IIPDataReceiveListener;
-import com.digi.xbee.api.models.IPMessage;
-
-public class MyTCPReceiveListener implements IIPDataReceiveListener   {
-
-	public void ipDataReceived(IPMessage ipMessage) {
-		// TODO Auto-generated method stub
-		System.out.format("Received TCP msg from %s >> '%s' \n", ipMessage.getIPAddress(), ipMessage.getDataString());
-		
-	}
-
-}
->>>>>>> bfc92b836a02d9ee1331989282de10f7752ca9e2
